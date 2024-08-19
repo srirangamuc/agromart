22f79e048f8a0ee27c3cb10074a4a79b6d756b9b
srirangamuc <uc3932@gmail.com>
2024-08-20 00:50:08 +0530

    Made the code in OOPS

--- models/userModel.js
+++ models/userModel.js
@@ -1,26 +1,43 @@
 const mongoose = require('mongoose');
 
-const userSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: true
-    },
-    email: {
-        type: String,
-        required: true,
-        unique: true
-    },
-    password: {
-        type: String,
-        required: true
-    },
-    role: {
-        type: String,
-        enum: ['admin', 'normal', 'farmer'],
-        default: 'normal'
+//Define the User Class
+class User{
+    //Constructor function for the user class
+    constructor(name,email,password,role='normal'){
+        this.name=name
+        this.email=email
+        this.password = password
+        this.role = role
     }
-});
+    //Method to get Mongoose Schema
+    static getSchema(){
+        return new mongoose.Schema({
+            name:{
+                type:String,
+                required:true
+            },
+            email:{
+                type:String,
+                required:true,
+                unique:true
+            },
+            password:{
+                type:String,
+                required:true
+            },
+            //their are three roles for our project
+            role:{
+                type:String,
+                enum :['admin','normal','farmer'],
+                default:'normal'
+            }
+        });
+    }
+    //Method to return the Mongoose Model
+    static getModel(){
+        return mongoose.model('User',User.getSchema());
+    }
+}
 
-const User = mongoose.model('User', userSchema);
-
-module.exports = User;
+//Export The User Model
+module.exports = User.getModel()
